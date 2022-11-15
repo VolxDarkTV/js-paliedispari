ac08e2d20cff4e6ecf925a984509dec2b572d17c
VolxDarkTV <vinvenzo.foti@outlook.it>
2022-11-15 16:46:44 +0100

    Add Palindroma function version

--- palindroma/index.js
+++ palindroma/index.js
@@ -1,31 +1,45 @@
-// Ciedere all'utente di inserire una parola
 
-// let parola = myFunction(prompt());
-
-// console.log(parola);
-// Realizzare una struttura che analizzi la parola per capire se è palindroma
-
-// function myFunction(palindromaTest) {
-//     let testoSplit = palindromaTest.split('');
-//     return palindromaTest;
-
-// }
 
 // Faccio inserire la parola all'utente
-let par = prompt();
-// splitto la parola
-let vatt = par.split('');
-console.log(vatt);
-// inverto le lettere della parola
-let parolaDivisa = vatt.reverse();
-console.log(parolaDivisa);
-// riunisco le lettere
-let parolaInvertita = parolaDivisa.join('');
-console.log(parolaInvertita);
-// Creo la condizione
-if(par == parolaInvertita){
-    console.log(`La parola ${par} è palindroma`);
-  } else {
-    console.log(`La parola ${par} non è palindroma`);
-  }
+// let par = prompt();
+// // splitto la parola
+// let vatt = par.split('');
+// console.log(vatt);
+// // inverto le lettere della parola
+// let parolaDivisa = vatt.reverse();
+// console.log(parolaDivisa);
+// // riunisco le lettere
+// let parolaInvertita = parolaDivisa.join('');
+// console.log(parolaInvertita);
+// // Creo la condizione
+// if(par == parolaInvertita){
+//     console.log(`La parola ${par} è palindroma`);
+//   } else {
+//     console.log(`La parola ${par} non è palindroma`);
+//   }
+
+
+myFunction()
+
+//   Versione in function
+function myFunction() {
+    // Faccio inserire la parola Desiderata
+    let par = prompt();
+    // Splitto la parola
+    let vatt = par.split('');
+    console.log(vatt);
+    // inverto le lettere della parola
+    let parolaDivisa = vatt.reverse();
+    console.log(parolaDivisa);
+    // riunisco le lettere
+    let parolaInvertita = parolaDivisa.join('');
+    console.log(parolaInvertita);
+    // Creo la condizione
+    if(par == parolaInvertita){
+        console.log(`La parola ${par} è palindroma`);
+    } else {
+        console.log(`La parola ${par} non è palindroma`);
+    }
+  
+}
   
